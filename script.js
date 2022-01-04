99d99c58e0f13a24da1ddee7d08eba8e56a48cd9
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-01-04 06:09:55 -0600

    small changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //When uses presses button rock shows, rock shows, computer choice shows. 
-const selectRock = document.getElementById('rock-button').addEventListener('click', (e) => {
+const selectRock = document.getElementById('rock').addEventListener('click', (e) => {
     document.getElementById('demo').innerHTML = '<img src=images/rock.jpg>';
     //The computer randomly chooses it selection choice. 
     //Display choice to human user. 
 });
 
-const selectPaper = document.getElementById('paper-button').addEventListener('click', (e) => {
+const selectPaper = document.getElementById('paper').addEventListener('click', (e) => {
     document.getElementById('demo').innerHTML = '<img src=images/paper.jpg>';
 });
 
-const selectScissors = document.getElementById('scissors-button')
-    .document.getElementById('demo').innerHTML = '<img src=images/scissors.jpg>';
-
+const selectScissors = document.getElementById('scissors').addEventListener('click', (e) => {
+    document.getElementById('demo').innerHTML = '<img src=images/scissors.jpg>';
+});
 
 const showHumanChoice = document.getElementById('human-select');
 const showComputerChoice = document.getElementById('computer-select');
@@ -21,15 +21,18 @@ const possibleChoices = document.querySelectorAll('button');
 let humanChoice;
 let computerChoice;
 
+
 possibleChoices.forEach(gameChoice => gameChoice.addEventListener('click', (e) => {
     humanChoice = e.target.id
-    showHumanChoice.innerHTML = humanChoice
+    showHumanChoice.innerHTML = `You chose: ${humanChoice}`
     generateComputerChoice()
 }))
 
+
+
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1
-
+    console.log(randomNumber)
     if (randomNumber === 1) {
         computerChoice = 'rock'
     }
@@ -39,12 +42,14 @@ function generateComputerChoice() {
     if (randomNumber === 3) {
         computerChoice = 'scissors'
     }
-    showComputerChoice.innerHTML = computerChoice;
+    showComputerChoice.innerHTML = `The computer chose: ${computerChoice}`;
 }
 
 
 
-//The computer will not pick anything without the human choice.
+
+
+
 
 
 
