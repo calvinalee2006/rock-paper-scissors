3b1fa8de1172e0f7edb32e5372d020a30be22af5
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-01-05 14:52:57 -0600

    small changes

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 //When uses presses button rock shows, rock shows, computer choice shows. 
-const selectRock = document.getElementById('rock').addEventListener('click', (e) => {
-    document.getElementById('demo').innerHTML = '<img src=images/rock.jpg>';
+const selectRock = document.getElementById('choices').addEventListener('click', (e) => {
+    document.getElementById('demo1').innerHTML = '<img src=images/rock.jpg>';
     //The computer randomly chooses it selection choice. 
     //Display choice to human user. 
 });
 
-const selectPaper = document.getElementById('paper').addEventListener('click', (e) => {
-    document.getElementById('demo').innerHTML = '<img src=images/paper.jpg>';
+const selectPaper = document.getElementById('choices').addEventListener('click', (e) => {
+    document.getElementById('demo1').innerHTML = '<img src=images/paper.jpg>';
 });
 
-const selectScissors = document.getElementById('scissors').addEventListener('click', (e) => {
-    document.getElementById('demo').innerHTML = '<img src=images/scissors.jpg>';
+const selectScissors = document.getElementById('choices').addEventListener('click', (e) => {
+    document.getElementById('demo1').innerHTML = '<img src=images/scissors.jpg>';
 });
 
 const showHumanChoice = document.getElementById('human-select');
 const showComputerChoice = document.getElementById('computer-select');
 const gameResult = document.getElementById('game-result');
-const possibleChoices = document.querySelectorAll('button');
+const possibleChoices = document.querySelectorAll('choices');
 
 let humanChoice;
 let computerChoice;
@@ -31,6 +31,7 @@ possibleChoices.forEach(gameChoice => gameChoice.addEventListener('click', (e) =
 
 
 function generateComputerChoice() {
+
     const randomNumber = Math.floor(Math.random() * 3) + 1
     console.log(randomNumber)
     if (randomNumber === 1) {
@@ -42,7 +43,9 @@ function generateComputerChoice() {
     if (randomNumber === 3) {
         computerChoice = 'scissors'
     }
-    showComputerChoice.innerHTML = `The computer chose: ${computerChoice}`;
+
+    computerChoice = document.getElementById('demo2').innerHTML = `The computer chose: ${computerChoice}`
+
 }
 
 
